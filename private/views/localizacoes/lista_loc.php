7e8685a0cd56d53f699d635702c99ce1ef0530e8
Diogo Almeida <1231184@isep.ipp.pt>
2026-06-12 19:00:27 +0100

    Implementacao completa de sessões, segurança de rotas privadas e autenticação

--- private/views/localizacoes/lista_loc.php
+++ private/views/localizacoes/lista_loc.php
@@ -1,3 +1,9 @@
+<?php
+require_once __DIR__ . '/../../includes/funcoes.php';
+redirect_if_not_logged();
+start_session();
+?>
+
 <?php include '../../includes/header.php'; ?>
 
     <?php include '../../includes/sidebar.php'; ?>
